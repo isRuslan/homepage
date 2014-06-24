b34e738cba2b02ed44c7330bb94b0723831d3db9
Ruslan Ismagilov <is.ismagilov@gmail.com>
2014-06-25 01:08:30 +0400

    web: fix server

--- web.js
+++ web.js
@@ -1,10 +1,6 @@
 var express = require("express");
 var app = express();
-app.use(express.logger());
 
 app.use('/', express.static('build/'));
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(process.env.PORT || 5000);
