bfba0efe6c76e57202adbfd248f9f003d053ae32
Ruslan Ismagilov <is.ismagilov@gmail.com>
2014-10-05 23:49:34 +0400

    App: disqus fix js

--- contents/scripts/init-disqus.js
+++ contents/scripts/init-disqus.js
@@ -1,8 +1,9 @@
+window.onload = function () {
+	var disqus_shortname = 'isruslan';
 
-var disqus_shortname = 'isruslan';
-console.log( 666 );
-(function() {
-    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-})();
+	(function() {
+	    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+	    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+	    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+	})();
+};
