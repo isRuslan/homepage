b96f7ed080ad6ad27b5a7630a37629ffe63675cd
Ruslan Ismagilov <is.ismagilov@gmail.com>
2015-01-05 21:54:20 +0300

    GH: git rm

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -14,7 +14,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
   #go into diractory and copy data we're interested in to that directory
   cd gh-pages
-  rm -rf *
+  git rm -rf *
   cp -Rf $HOME/build/* .
 
   #add, commit and push files
