329534e85564cfb98c7ffe56163bbf64efbebcc2
Ruslan Ismagilov <ruslan@seedr.ru>
2015-04-17 18:21:54 +0300

    deploy: add gh-pages

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+cd build
+git init
+
+git config user.name "Travis CI"
+git config user.email "travis@travis-mail"
+
+git add -A
+git commit -m "Deploy to GitHub Pages"
+
+# Force push from the current repo's master branch to the remote repo's gh-pages branch.
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
