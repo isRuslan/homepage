51643012779283a89e706d36528fb56d9eea81f0
Ruslan Ismagilov <ruslan@seedr.ru>
2015-04-17 22:23:36 +0300

    deploy: fixes

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ git add -A
 git commit -m "Deploy to GitHub Pages"
 
 # Force push from the current repo's master branch to the remote repo's gh-pages branch.
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" main:master > /dev/null 2>&1
+git push --force "https://${GH_TOKEN}@${GH_REF}" main:master > /dev/null 2>&1
